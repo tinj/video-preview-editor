663f99a018b95ee3b54b5c21313846792fcb16ae
Becka <rebecca.robbins.et@gmail.com>
2014-09-16 16:08:36 -0700

    started to refactor closure functions.  removed some excessive jquery

--- imageselect.js
+++ imageselect.js
@@ -1,5 +1,11 @@
 /* global $, _, IScroll */
 
+
+/**
+ * jfdsafjkl;
+ * jfewioao
+ */
+
 /////////////////////////////////////////////
 //////    initializations              //////
 /////////////////////////////////////////////
@@ -19,10 +25,11 @@ Thumbnails.prototype.initialize = function(){
     this.defineCachedJqueryVars();
     this.initializeImageArrays();
     this.updateHtmlListInFirstScroller();
-    clickFirstImageArrayToGetImage(this);
+    this.clickFirstImageArrayToGetImage();
     this.determineWhichScreenToStart();
 };
 
+//refresh iscroll whenever content is replaced thumbnails.myScroll2.refresh()
 //iscroll. fameo.us later?
 Thumbnails.prototype.initializeImageArrays = function(){
     var settings = {
@@ -65,11 +72,11 @@ Thumbnails.prototype.updateHtmlListInFirstScroller = function(){
         this.newArrayForSelection = this.newFramesetFromWholeSet();
     }
     // FIXME - does not want variable
-    replaceList($("#scroller"), this.newArrayForSelection);
+    replaceList(this.$scroller, this.newArrayForSelection);
 };
-
+    // FIXME - does not want variable
 Thumbnails.prototype.updateHtmlListInSecondScroller = function(){
-    replaceList($("#scroller2"), this.arrayOfImagesToSelectFrom);
+    replaceList(this.$scroller2, this.arrayOfImagesToSelectFrom);
 };
 
 //maps images to html
@@ -105,36 +112,39 @@ Thumbnails.prototype.newFramesetFromWholeSet = function(){
 
 //handle click event for first array to generate second array
 
-// make prototype
-//FIXME - refactor to cached jquery
-function clickFirstImageArrayToGetImage(thumbnails){
+//FIXME - once in Thumbnail prototype, remove selected_frame from $initialLi
+Thumbnails.prototype.clickFirstImageArrayToGetImage = function(){
     console.log("clickFirstImageArrayToGetImage function");
-    $("#scroller img").click(function(){
-        thumbnails.$scroller2.show();
-        var $initialImage = $(this);
-        var $initialLi = $initialImage.parent();
-        var initialIscrollId = $initialLi.parent().parent().attr('id');
-        $initialLi.addClass('selected_frame');
-        var initialImgUrl = $initialImage.attr('src');
-        if (thumbnails.$initialLi){
-            thumbnails.$initialLi.removeClass('selected_frame');
-            if(thumbnails.$initialImgUrl  === initialImgUrl){
-            }
-        }
-        thumbnails.$initialLi = $initialLi;
-        thumbnails.initialImgUrl = initialImgUrl;
-        thumbnails.getNeighboringImages(thumbnails.imgUrlFirst, 7);
-    });
+    this.$scroller.find("img").click(somethingClicked.bind(this));
+};
+
+function somethingClicked (evt) {
+    var $el = $(evt.target);
+    // console.log($el);
+    this.$scroller2.show();
+    var $clickedLi = $el.parent();
+    // this.initialIscrollId = $clickedLi.parent().parent().attr('id');
+    $clickedLi.addClass('selected_frame');
+    var clickedImgUrl = $el.attr('src'); //not defined
+    console.log(clickedImgUrl);
+    if (this.$initialLi){
+        this.$initialLi.removeClass('selected_frame');
+    }
+    this.$initialLi = $clickedLi;
+    this.initialImgUrl = clickedImgUrl;
+    this.getNeighboringImages(7);
 }
 
+//TEST ME
 //for second iscroll
-Thumbnails.prototype.getNeighboringImages = function(imageFrame, arrayLength){
+Thumbnails.prototype.getNeighboringImages = function(arrayLength){
     console.log("getNeighboringImages function");
     this.openImageSelector("#scroller2");
-    this.index = _.indexOf(this.initialLargeArrayOfImages, this.imgUrlFirst, this);
-    this.half_array_length = Math.ceil(arrayLength /2);
-    var lowerBound = Math.max(this.index- this.half_array_length, 0);
-    var upperBound = Math.min(this.index+ this.half_array_length, this.initialLargeArrayOfImages.length-1);
+    var index = _.indexOf(this.initialLargeArrayOfImages, this.initialImgUrl, this);
+    console.log(index);
+    var half_array_length = Math.ceil(arrayLength /2);
+    var lowerBound = Math.max(index- half_array_length, 0);
+    var upperBound = Math.min(index+ half_array_length, this.initialLargeArrayOfImages.length-1);
     if(lowerBound === 0){
         upperBound = arrayLength;
     }
@@ -150,7 +160,8 @@ Thumbnails.prototype.getNeighboringImages = function(imageFrame, arrayLength){
 Thumbnails.prototype.selectReplacementImage = function(){
     console.log("selectReplacementImage function");
     //FIXME: does not work if $("#scroller2 li > img") is replaced with this.$imagesInScroller
-    $("#scroller2 li > img").click(function(){
+    this.$scroller2.find("li > img").click(function(evt){
+        var $el = $(evt.target);
         console.log("selectReplacementImage");//not called
         this.replacementImage = $(this);
         this.$replacementLi = this.replacementImage.parent();
@@ -167,6 +178,7 @@ Thumbnails.prototype.selectReplacementImage = function(){
                 return this.updateOriginal();
             }
         }
+
         this.$replacementLi = $replacementLi;
         this.replacementImgUrl = replacementImgUrl;
     }.bind(this));
@@ -189,12 +201,61 @@ Thumbnails.prototype.generateNewArrayOfImagesFromModifiedScroller = function(){
 //replace frame and close second iscroll
 Thumbnails.prototype.updateOriginal = function(){
     this.$initialLi.find('img').attr('src', this.replacementImgUrl);
-    this.$replacementImgUrl = undefined;
-    thisreplacementImage_url = undefined;
-    this.replacementLi = undefined;
+    this.replacementImgUrl = undefined;
+    this.$replacementLi = undefined;
     thumbnails.closeImageSelector("#scroller2");
     return this.generateNewArrayOfImagesFromModifiedScroller();
 };
+
+Thumbnails.prototype.serialize = function () {
+    return {
+        newArrayForSelection: this.newArrayForSelection
+    };
+};
+
+/////////////////////////////////////////////
+//////  first page form processing     //////
+/////////////////////////////////////////////
+
+//has defaults
+Thumbnails.prototype.get_form_values = function(){
+    this.base_url = $("#baseurl").val();
+    this.url_extension = "."+$("#url_extension").val();
+    this.num_of_frames =parseInt($("#total").val());
+    this.end_frame =parseInt($("#end_frame").val());
+    this.start_frame = parseInt($("#start_frame").val());
+    this.step = parseInt($("#step").val());
+    this.frame_label_length = parseInt($("#frame_label_length").val());
+};
+
+//create iscroll
+Thumbnails.prototype.create_string_with_zeros = function(frame_number){
+    var string_frame = ""+ frame_number;
+    var length_of_number = string_frame.length;
+    var required_string_length = parseInt(this.frame_label_length)-length_of_number;
+    for(var i=0; i< required_string_length; i++){
+        string_frame="0"+string_frame;
+    }
+    return string_frame;
+};
+
+//maps create_string function to range of frames
+Thumbnails.prototype.create_padded_range = function(){
+    this.frame_nums = _.range(this.start_frame, this.end_frame, this.step);
+    return _.map(this.frame_nums, function(frame_number){
+        //undefined
+        return this.create_string_with_zeros(frame_number);
+    }, this);
+};
+
+//maps new url strings to specified params.
+Thumbnails.prototype.create_array_of_urls = function(){
+    this.padded_frame_nums = this.create_padded_range();
+    return _.map(this.padded_frame_nums, function(frame_number){
+        return this.base_url+frame_number+this.url_extension;
+    }, this);
+};
+
 
 /////////////////////////////////////////////
 //////    jquery click events           //////
@@ -210,6 +271,8 @@ Thumbnails.prototype.defineCachedJqueryVars =function(){
     this.$scroller2 = $("#scroller2");
     this.$currentUrl = $("#currentUrl");
     this.$imagesInScroller = $("#scroller2 li > img");
+
+    // this.$scroller2.find('li > img')
     // this.$scroller2Img = $("#scroller2 li > img");
 };
 
@@ -217,16 +280,19 @@ Thumbnails.prototype.defineCachedJqueryVars =function(){
 Thumbnails.prototype.navToFirst = function(){
     this.showFirstPage.click(function(){
         this.$secondModal.modal("hide");
-       this.$firstModal.modal("show");
-    this.firstSubmitButton();
+        this.$firstModal.modal("show");
+        this.firstSubmitButton();
     }.bind(this));
 };
 
 //first screen form
-Thumbnails.prototype.firstSubmitButton = function(){
-    this.submitButton.click(function(){
-        this.$firstModal.modal("hide");
-        this.$secondModal.modal("show");
+Thumbnails.prototype.first_submit_button = function(){
+    $("#submit_butn").click(function(){
+        this.get_form_values();
+        this.create_initial_li();
+        click_first_iscroll_to_get_image(this);
+        $("#firstModal").modal("hide");
+        $("#secondModal").modal("show");
     }.bind(this));
 };
 
@@ -241,10 +307,13 @@ Thumbnails.prototype.launchFirstModalScreen = function(){
     firstSubmitButton();
 };
 
+//FIXME remove
 Thumbnails.prototype.closeImageSelector = function(id) {
     $(id).hide();
 };
 
+
+//FIXME remove
 Thumbnails.prototype.openImageSelector = function(id){
     $(id).show();
 };
