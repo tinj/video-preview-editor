a676467b16380d6d40e7839c2e778efe93724596
Becka R. <rebecca@rebecca-robbins.com>
2014-12-30 16:27:07 -0800

    added comments and removed unnecessary functions

--- gulpfile.js
+++ gulpfile.js
@@ -16,15 +16,12 @@ var WebpackDevServer = require('webpack-dev-server');
 var watch            = require('gulp-watch');
 
 var config = {
-    // consts: {
-    //     defaults: {
-    //     }
-    // },
     clean: {
         defaults: {
             input: path.join(__dirname, '/build/*')
         }
     },
+    
     // examples: {
     //     defaults: {
     //         input: path.join(__dirname, '/examples/src/**/*.html.jade'),
@@ -33,12 +30,7 @@ var config = {
     //         },
     //         output: path.join(__dirname, '/examples')
     //     }
-    // },
-    // test: {
-    //     defaults:  {
-    //         input: path.join(__dirname, '/test/**/*.spec.js')
-    //     }
-    // },
+    // }, 
     uglify: {
         defaults: {
             input: path.join(__dirname, '/build/*.js'),
@@ -47,54 +39,12 @@ var config = {
         }
     },
     webpack: require('./webpack.conf.js')
-    // webpackDevServer: {
-    //     defaults: {
-    //         // contentBase: path.join(__dirname, '/examples'),
-    //         // hot: true,
-    //         noInfo: true,
-    //         stats: {
-    //             colors: true
-    //         }
-    //     }
-    // }
 };
 
-gulp.task('build', [
-    'uglify'
-]);
+//by default bundle with webpack and create uglify version 
+gulp.task('default', ['webpack', 'uglify']);
 
-// gulp.task('ci', function() {
-//     var opts = _.clone(config.test.defaults);
-//     return gulp.src(opts.input)
-//         .pipe(karma({
-//             action: 'watch',
-//             configFile: 'karma.conf.js'
-//         }))
-//         .on('error', function(err) {
-//             // Make sure failed tests cause gulp to exit with a non-zero status
-//             throw err;
-//         });
-// });
-
-
-//converts from jade to html - runs it once, not dynamically
-// gulp.task('templates', function(){
-//     // gulp.src('templates/*.jade');
-//     // .pipe(watch())
-//     // .pipe(jade())
-//     // .pipe(gulp.dest("build"));
-//     // .pipe(livereload());
-// });
-
-//converts from less to css - runs it once, not dynamically
-// gulp.task('less', function(){
-//     gulp.src('styles/*.less')
-//     .pipe(watch())
-//     .pipe(less())
-//     .pipe(gulp.dest("build/css"));
-//     // .pipe(livereload());
-// });
-
+//get rid of everything in the build folder because a new version is created
 gulp.task('clean', function() {
     var opts = _.clone(config.clean.defaults);
     return gulp.src(opts.input, {read: false})
@@ -105,8 +55,6 @@ gulp.task('watch', function(){
     livereload.listen();
     gulp.watch('build/**').on('change', livereload.changed);
 });
-
-gulp.task('default', ['webpack', 'uglify']);
 
 gulp.task('examples', function() {
     var opts = _.clone(config.examples.defaults);
@@ -132,6 +80,7 @@ gulp.task('examples', function() {
 //         });
 // });
 
+//makes minified version of webpack build 
 gulp.task('uglify', ['clean','webpack'], function() {
     var opts = _.clone(config.uglify.defaults);
     return gulp.src(opts.input)
@@ -140,33 +89,34 @@ gulp.task('uglify', ['clean','webpack'], function() {
         .pipe(gulp.dest(opts.output));
 });
 
-gulp.task('webpack-dev-server', function() {
-    var opts = _.merge(config.webpack.standalone, config.webpack.defaults, _.defaults);
-    var serverOpts = _.clone(config.webpackDevServer.defaults);
-    opts.cache = true;
-    opts.devtool = 'eval';
-    opts.debug = true;
-    opts.output.pathinfo = true;
-    opts.watch = true;
-    opts.entry = [
-        'webpack-dev-server/client?http://localhost:3232',
-        // 'webpack/hot/dev-server',
-        opts.entry
-    ];
+// gulp.task('webpack-dev-server', function() {
+//     var opts = _.merge(config.webpack.standalone, config.webpack.defaults, _.defaults);
+//     var serverOpts = _.clone(config.webpackDevServer.defaults);
+//     opts.cache = true;
+//     opts.devtool = 'eval';
+//     opts.debug = true;
+//     opts.output.pathinfo = true;
+//     opts.watch = true;
+//     opts.entry = [
+//         'webpack-dev-server/client?http://localhost:3232',
+//         // 'webpack/hot/dev-server',
+//         opts.entry
+//     ];
     // opts.profile = true;
 
-    opts.plugins = opts.plugins.concat(
-        new webpack.DefinePlugin(config.consts.defaults)
-    );
+    // opts.plugins = opts.plugins.concat(
+    //     new webpack.DefinePlugin(config.consts.defaults)
+    // );
 
-    var compiler = webpack(opts);
-    new WebpackDevServer(compiler, serverOpts).listen(3232, 'localhost', function(err) {
-        if (err) throw new util.PluginError('webpack-dev-server', err);
-        util.log('[webpack-dev-server]', 'Development server is running.');
-        util.log('[webpack-dev-server]', 'View examples at http://localhost:3232/examples/');
-    });
-});
+//     var compiler = webpack(opts);
+//     new WebpackDevServer(compiler, serverOpts).listen(3232, 'localhost', function(err) {
+//         if (err) throw new util.PluginError('webpack-dev-server', err);
+//         util.log('[webpack-dev-server]', 'Development server is running.');
+//         util.log('[webpack-dev-server]', 'View examples at http://localhost:3232/examples/');
+//     });
+// });
 
+//bundle with webpack; runs clean dependency first 
 gulp.task('webpack', ['clean'], function(cb) {
     var opts = _.clone(config.webpack);
 
